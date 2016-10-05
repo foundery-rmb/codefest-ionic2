553f5066feab533d10e567d80eb546bbec89c114
RDB1990 <rachanab@letsema.co.za>
2016-10-05 14:17:47 +0200

    Remove page 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { StatusBar } from 'ionic-native';
 import './rxjs-operators';
 
 import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +21,6 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { HttpModule, JsonpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
 import { PingComponent } from './ping/ping.component';
 import { PingService } from './ping/ping.service'
 
@@ -13,18 +12,15 @@ import { PingService } from './ping/ping.service'
     MyApp,
     PingComponent,
     Page1,
-    Page2
   ],
   imports: [
     IonicModule.forRoot(MyApp),
     HttpModule,
-    JsonpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     Page1,
-    Page2
   ],
   providers: [PingService]
 })
