fb9f264e582b435e9d83a2cd7916a674bc74e5e0
Niel de Wet <niel.dewet@bsg.co.za>
2016-10-06 23:11:34 +0200

    Count and display count of fund types for a client

--- src/components/client-funds/client-funds.ts
+++ src/components/client-funds/client-funds.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 import { ClientFund } from '../../app/services/alice.response/client-fund';
 
@@ -6,7 +6,32 @@ import { ClientFund } from '../../app/services/alice.response/client-fund';
   selector: 'client-funds',
   templateUrl: 'client-funds.html'
 })
-export class ClientFunds {
+export class ClientFunds implements OnChanges {
 
   @Input() funds: ClientFund[];
+
+  countedFunds: { key: string; value: number }[] = [];
+
+  ngOnChanges(changes: SimpleChanges) {
+    let funds = changes['funds'];
+    if (!funds.currentValue) {
+      return;
+    }
+
+    let countedFunds: { [key: string]: number } = {};
+    for (let fund of funds.currentValue) {
+      if (fund.Legal_persona_fund.length === 0) {
+        continue;
+      }
+      if (!countedFunds[fund.Legal_persona_fund]) {
+        countedFunds[fund.Legal_persona_fund] = 1;
+      } else {
+        countedFunds[fund.Legal_persona_fund] += 1;
+      }
+    }
+
+    for (let fundKey in countedFunds) {
+      this.countedFunds.push({ key: fundKey, value: countedFunds[fundKey] });
+    }
+  }
 }
