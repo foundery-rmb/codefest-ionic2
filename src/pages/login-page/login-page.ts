5335246d84c924742c09fd66dc92c2a9c1e58d9d
Niel de Wet <niel.dewet@bsg.co.za>
2016-10-07 07:46:59 +0200

    Add navigation from mock login to welcome page

--- src/pages/login-page/login-page.ts
+++ src/pages/login-page/login-page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { WelcomePage } from '../welcome/welcome';
+
 @Component({
   selector: 'page-login-page',
   templateUrl: 'login-page.html'
@@ -8,4 +10,8 @@ import { NavController } from 'ionic-angular';
 export class LoginPage {
 
   constructor(public navCtrl: NavController) {}
+
+  navigateForward() {
+    this.navCtrl.push(WelcomePage);
+  }
 }
