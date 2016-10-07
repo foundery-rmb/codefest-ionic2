4f1039a03820399c4032c0f5b78666fb700cb92f
Niel de Wet <niel.dewet@bsg.co.za>
2016-10-07 08:58:54 +0200

    Remove redundant get

--- src/app/alice-input/alice-input.component.ts
+++ src/app/alice-input/alice-input.component.ts
@@ -17,11 +17,6 @@ export class AliceInputComponent {
     constructor(private navCtrl: NavController, private aliceQueryService: AliceQueryService) { }
 
     onsubmit() {
-        // TODO : Remove showing response on welcome page
-        this.aliceQueryService.query(this.userQuery)
-            .subscribe(
-            aliceResponse => this.aliceResponse = aliceResponse,
-            error => console.log(error));
         this.navCtrl.push(ClientProfile, { observable: this.aliceQueryService.query(this.userQuery) });
     }
 }
